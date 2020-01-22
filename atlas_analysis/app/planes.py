31a739929bc6bdbd218d559d3baa14239b08589b
Luc Guyot <luc.guyot@epfl.ch>
2020-01-22 09:25:19 +0100

    Fixes the display of the help information for the function creating the centerline and its orthogonal planes [BBPP42-462]
    * Because of the indentation, the automatic paragraph wrapping of click was not performed properly.
    In particular some wide spaces were introduced between consecutive words of sentences
    and most of the line feeds were skipped.
    * Few minor changes of the help text have been done.
    * The documentation of the draw function has been expanded to document its two parameters.
    For this, we followed the unix-like documentation schema presented as the click implicit standard in
    Evidlo's comment to this github issue: https://github.com/pallets/click/issues/587.
    
    Change-Id: I6c922ca6e57362c06a2645b653e301a05a50946c

--- atlas_analysis/app/planes.py
+++ atlas_analysis/app/planes.py
@@ -18,7 +18,7 @@ def app(verbose):
 
 @app.command()
 @click.argument('nrrd_path', type=FILE_TYPE)
-@click.option('-o', '--output', type=str, help='Output file containing centerline/planes files',
+@click.option('-o', '--output', type=str, help='Output file containing centerline/planes',
               required=True)
 @click.option('-s', '--start', type=str, help='The starting voxel (use itk-snap to define it)',
               required=True)
@@ -48,43 +48,48 @@ def app(verbose):
 @log_args(L)
 def create(nrrd_path, output, start, end, downhill, chain_length, chain_count, sampling,
            link_distance, plane_count, seed):
-    """ Run the plane creation for a given atlas (brain_region nrrd file)
+    """ Run the plane creation for a given atlas (brain_region nrrd file).
 
-    Notes:
-        The algorithm will do the following steps (the related input variables are put
-        between parenthesis):
-          - compute the volume distance transform (nrrd_path)
-          - use multiple stochastic chains to sample the distance transform distribution ridge
-          ie: local minimums along the volume.
-            - user defines two entry points for the volume. ie: the centerline will go trough these
-              points. (starting_points)
-            - stochastic chains of a certain length (chain_length) are launched from these points.
-              They will climb up the distance transform distribution with a high probability.
-              With a small probability they will go downhill. This allows the chains to escape from
-              local maximums 'hills' (downhill)
-            - the volume can have multiple ridges and having multiple chains reduce the risk of
-              missing some of them. (chain_count)
-            - we sample the chain to reduce the autocorrelation of the chains (sampling)
-          - the amount of points produced by the chains where the distance transform is high can
-            be important so we do a clustering of points merging all the points that are in
-            the range of voxel dimension.
-          - we create a graph from these remaining points
-            - nodes are point locations from the chains/clustering part
-            - edges are created between close enough points (link_distance)
-            - we need to find a path between the two starting points, this will define the
-              centerline but it appears that the graph is not always connected. So we iteratively
-              connect the graph by connecting the closest connected components first.
-          - we do a shortest path research in the graph between the two starting points using a
-            weighted Dijkstra. This creates the centerline.
-          - the centerline usually jitter a lot and needs to be smoothed.
-            - we use a bezier curve to do the smoothing with multiple control
-              points (ctrl_point_count)
-            - control points are taken from the shortest path points.
-          - we create a parametric spline function on top of the bezier curve
-          - derivative of this curve corresponds to the plane orientations (plane_count)
-            - we take two points that are close on the spline. The vector between these points is
-              define the normal of the local perpendicular plane.
-          - save the results (output_path)
+    This function creates the centerline of the input volume together with
+    a series of planes orthogonal to this curve. The number of planes to create
+    is specified with the plane_count argument. The output of the function is an .npz file
+    bearing the specified name. This file can be loaded subsequently with numpy.load().
+
+    Notes:\n
+        The algorithm will do the following steps
+    (the related input variables are put between parentheses):\n
+          - Compute the distance transform of the input volume (nrrd_path).\n
+          - Use multiple stochastic chains to sample the distance transform distribution ridge,
+    i.e., local maxima along the volume.\n
+            - The user defines two entry points for the volume, i.e., the centerline
+    will go through these points (starting_points).\n
+            - Stochastic chains of a certain length (chain_length) are launched from these points.
+    They will climb up the distance transform distribution with a high probability.
+    With a small probability they will go downhill. This allows the chains to escape from
+    local maxima 'hills' (downhill).\n
+            - The volume can have multiple ridges and having multiple chains reduce the risk of
+    missing some of them (chain_count).\n
+          - We sample the chain to reduce the autocorrelation of the chains (sampling).\n
+          - The amount of points produced by the chains where the distance transform is high can
+    be important so we do a clustering of points merging all the points that are in
+    the range of the voxel dimension.\n
+          - We create a graph from these remaining points:\n
+            - Nodes are point locations from the chains/clustering part.\n
+            - Edges are created between close enough points (link_distance).\n
+          - We need to find a path between the two starting points. This will define the
+    centerline but it appears that the graph is not always connected. So we iteratively
+    connect the graph by connecting the closest connected components first.\n
+          - We do a shortest path search in the graph between the two starting points using a
+    weighted Dijkstra. This creates the centerline.\n
+          - The centerline usually jitters a lot and needs to be smoothed.\n
+            - We use a Bézier curve to do the smoothing with multiple control
+    points (ctrl_point_count).\n
+            - Control points are taken from the shortest path points.\n
+          - We create a parametric spline function on top of the Bézier curve.\n
+          - The derivative of this curve corresponds to the plane normal (plane_count).\n
+            - We take two points that are close on the spline. The vector between these points
+    defines the normal of the local perpendicular plane.\n
+          - Save the results (output_path).\n
     """
     bounds = [split_str(start, int), split_str(end, int)]
     planes.create_centerline_planes(nrrd_path, output, bounds,
@@ -99,5 +104,14 @@ def create(nrrd_path, output, start, end, downhill, chain_length, chain_count, s
 @click.argument('preprocess_path', type=FILE_TYPE)
 @click.option('-n', '--name', type=str, help='The data set name', required=True)
 def draw(nrrd_path, preprocess_path, name):
-    """ Draw the result of the plane creation on top of the atlas"""
+    """ Draw the result of the plane creation on top of the atlas.
+
+      Usage: atlas-analysis planes draw [OPTIONS] NRRD_PATH PREPROCESS_PATH\n
+      Draw the centerline and its orthogonal planes on top of the
+      corresponding volumetric file.
+      NRRD_PATH is the path to the volumetric file used to generate the
+      centerline and its orthogonal planes.
+      PREPROCESS_PATH is the path to the .npz file containing the centerline
+      and the orthogonal planes data.
+    """
     planes.draw(nrrd_path, preprocess_path, name)
